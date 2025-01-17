e507a37168e269acf59d47b198d5ace50d35d0ce
emazupp <zuppardo.emanuele2001@gmail.com>
2025-01-17 10:08:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./assets/css/App.css";
+
+function App() {
+  return (
+    <>
+      <h1>ciao</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import "./assets/css/index.css";
+import App from "./App.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+
+createRoot(document.getElementById("root")).render(<App />);
